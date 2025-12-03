144fa2ac639ed3a9c46699eeec9bc7f4af7acda2
suyeon <stellajeong6@naver.com>
2025-12-03 11:19:17 +0900

    Revert "Update sort_files.sh"
    This reverts commit 283fcabe777f3e1e90e01b94b64c635a17811d6d.

--- sort_files.sh
+++ sort_files.sh
@@ -4,19 +4,12 @@ TARGET_DIR="$1" #첫번째 인자로 전달된 대상 폴더 경로를 변수에
 
 #폴더 경로를 입력하지 않은 경우
 if [ -z "$TARGET_DIR" ]; then
-    #사용자에게 정리할 폴더 경로를 직접 물어보고 TARGET_DIR에 저장
-    echo -n "정리할 폴더 경로를 입력하세요."
-    read TARGET_DIR
-
-    #사용자 입력 후에도 경로가 여전히 비어 있다면 종료
-    if [ -z "$TARGET_DIR" ]; then
-        echo "경로가 입력되지 않아 종료합니다."
-        exit 1
-    fi
+    echo "정리할 폴더 경로를 입력하세요."
+    exit 1
 fi
 
 #입력받은 대상 폴더로 이동, 실패 시 종료
-cd "$TARGET_DIR" || { echo "해당 폴더로 이동할 수 없습니다. 경로: $TARGET_DIR"; exit 1; }
+cd "$TARGET_DIR" || { echo "해당 폴더로 이동할 수 없습니다."; exit 1; }
 
 #현재 폴더의 모든 파일을 대상으로 반복 수행
 for file in *; do
